800592b540adb974a534aea5835afbb3c8c3afa2
Dmitry Sam <dmitry.sam88@gmail.com>
2018-09-09 16:36:30 +0300

    0.01

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactMde, {ReactMdeTypes} from 'react-mde';
+import * as Showdown from 'showdown';
+
+
+export interface AppState {
+    mdeState: ReactMdeTypes.MdeState;
+}
+
+class App extends Component {
+
+    converter: Showdown.Converter;
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+            mdeState: {
+
+                markdown: '**Hello world!**',
+
+            },
+
+        };
+
+        this.converter = new Showdown.Converter({
+
+            tables: true,
+
+            simplifiedAutoLink: true,
+
+            strikethrough: true,
+
+            tasklists: true,
+
+        });
+
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+          <ReactMde
+              onChange={this.handleValueChange}
+              editorState={this.state.mdeState}
+              generateMarkdownPreview={(markdown) => Promise.resolve(this.converter.makeHtml(markdown))}
+          />
+      </div>
+    );
+  }
+}
+
+export default App;
